0bac83df340af2888ea954848213179d7c4fb0a5
avaerian <55154263+avaerian@users.noreply.github.com>
2026-01-20 04:30:06 -0500

    Add Google Sans Code monospaced font

--- app/app.vue
+++ app/app.vue
@@ -7,5 +7,5 @@
 </template>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,1..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&family=Google+Sans+Flex:opsz,wght@6..144,1..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 </style>
