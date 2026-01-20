e4c1c85cb9397bfbc33623547546cdea537840e5
avaerian <55154263+avaerian@users.noreply.github.com>
2026-01-20 04:30:56 -0500

    Create about.vue and docs.vue

---
+++ app/pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <section class="intro">
+            <div class="container nav-width-size">
+                <div class="content">
+                    <h1>About Me</h1>
+                </div>
+
+            </div>
+        </section>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
