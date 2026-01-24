6bb4cc8034d6b7684ad8ad2d21357f4ff7972549
avaerian <55154263+avaerian@users.noreply.github.com>
2026-01-24 15:21:24 -0500

    Use computed style or CSS

---
+++ app/components/circle.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+    export default {
+        name: 'Circle',
+        props: {
+            size: {
+                type: String,
+            },
+            circleStyle: {
+                type: String,
+                default: "",
+            },
+            zIndex: {
+                type: Number,
+                default: 10,
+            }
+        },
+        computed: {
+            cssVars() {
+                return {
+                    '--size': this.size,
+                    '--circleStyle': this.circleStyle,
+                    '--zIndex': this.zIndex,
+                }
+            }
+        }
+    };
+</script>
+
+<template>
+    <div class="circle" :style="'--size:' + size + '; ' + circleStyle"></div>
+</template>
+
+<style scoped>
+    .circle {
+        position:absolute;
+        border-radius:50%;
+        box-shadow: inset 0px 0px 50px -12px rgba(129, 129, 129, 0.208);
+        z-index: var(--zIndex);
+        width: var(--size);
+        height: var(--size);
+    }
+</style>
