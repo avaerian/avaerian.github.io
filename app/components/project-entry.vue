1ba9b7bb694b5e826b38774003e9608083e6ed05
avaerian <55154263+avaerian@users.noreply.github.com>
2026-01-24 15:24:05 -0500

    Add classes to img and desc + img width var

--- app/components/project-entry.vue
+++ app/components/project-entry.vue
@@ -10,16 +10,20 @@
 
 <template>
     <div class="project-entry">
-        <img :src="img">
+        <img class="project-img" :src="img">
         <h2>{{ name }}</h2>
         <div class="project-entry-bottom">
-            <p>{{ desc }}</p>
+            <p class="desc">{{ desc }}</p>
             <a :href="link">Check it out <Icon name="material-symbols:arrow-outward-rounded" /></a>
         </div>
     </div>
 </template>
 
 <style>
+    :root {
+        --project-img-width: 360px;
+    }
+
     .project-entry {
         padding: 20px;
         -webkit-box-shadow: 0px 6px 22px -1px rgba(0, 0, 0, 0.349);
@@ -30,14 +34,23 @@
         background: rgba(227, 227, 227, 0.135);
         backdrop-filter: blur(12px);
         height: fit-content;
+        max-width: var(--project-img-width);
     }
     
     .project-entry:hover {
         transform: scale(1.05);
     }
+
+    .project-img {
+        transition: 0.3s all;
+    }
+
+    /*.project-entry:hover .project-img {
+        transform:scale(1.05);
+    }*/
     
     .project-entry>img {
-        width: 360px;
+        width: var(--project-img-width);
         border-radius: 12px;
         margin-bottom: 20px;
     }
@@ -51,9 +64,14 @@
         flex-direction: row;
     }
 
+    .desc {
+        overflow-wrap: normal;
+    }
+
     .project-entry-bottom>a {
         position: absolute;
         right: 20px;
+        bottom: 20px;
     }
 
     .project-entry-bottom>a>Icon {
