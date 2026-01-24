ac3636220c0cbf0e38b7b168bda4c8bfe3ccff77
avaerian <55154263+avaerian@users.noreply.github.com>
2026-01-24 15:21:51 -0500

    Add midnight arrives debug photo to front page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page-content">
-        <section class="intro">
+        <section id="intro">
             
             <div class="container nav-width-size">
                 <div class="content">
@@ -10,34 +10,49 @@
             </div>
         </section>
         
-        <section class="more">
+        <section id="more">
 
             <div class="container nav-width-size">
-                <div class="circle c1" style="left: -170px; top: 100px;"></div>
+                
                 <div class="cool-tag">1.</div>
-                <div class="content">
-                    <h1>Hello, world!</h1>
-                    <p>Goodbye, world</p>
-                    <p>Goodbye, world</p>
-                    <p>Goodbye, world</p>
-                    <p>Goodbye, world</p>
-                    <a href="/blog">Go to blog</a>
-                    <a href="/blog">Go to blog</a>
+                <div class="precontent">
+                    <!-- TODO: restructure to allow certain overflow items -->
+                    <Circle size="120px", 
+                        circleStyle="background:linear-gradient(135deg, #ceffad 1%,#2ad39e 100%); right: calc(var(--size) * -.4); top: calc(var(--size) * -.4);" />
+                    <div class="content">
+                    
+                        <div class="content-left">
+                            <h1>Hello, world!</h1>
+                            <p>Hello, I am Avaerian, more namely Gabriel Simon, and welcome to my website!</p>
+                            
+                            <p>Goodbye, world</p>
+                            <p>Goodbye, world</p>
+                            <p>Goodbye, world</p>
+                            <a href="/blog">Go to blog</a>
+                        </div>
+
+                        <div class="content-right">
+                            <img src="assets/midnight_arrives_debug_photo.png" style="border-radius: 24px">
+                        </div>
+                    </div>
                 </div>
 
             </div>
 
         </section>
         
-        <section class="more2">
+        <section id="projects">
             <div class="container nav-width-size">
                 <div class="content">
-                    <h2>Explore projects</h2>
-                    <div class="projects">
+                    <span class="projects-header">
+                        <h2>Explore projects</h2>
+                        <a href="/projects"><p>Explore more</p><Icon class="explore-btn-icon" name="material-symbols:arrow-forward-rounded"/></a>
+                    </span>
+                    <div class="project-entries">
                         <ProjectEntry img="https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/store/software/switch/70010000000964/a28a81253e919298beab2295e39a56b7a5140ef15abdb56135655e5c221b2a3a"
                         name="Ether" desc="A skyblock project" link="https://github.com/avaerian/Ether"></ProjectEntry>
-                        <ProjectEntry img="https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/store/software/switch/70010000000964/a28a81253e919298beab2295e39a56b7a5140ef15abdb56135655e5c221b2a3a"
-                        name="Hello, world!" desc="Goodbye, world!" link="https://github.com/avaerian/Ether"></ProjectEntry>
+                        <ProjectEntry img="https://cms-media.roblox.com/resize=width:1280,fit:max/GnaqqRBTcCuoRELL938w"
+                        name="Midnight Arrives" desc="Horror game with the goal of completing tasks to escape amidst the arachnae." link="https://www.roblox.com/games/7835879494/Midnight-Arrives-Asylum-Rework"></ProjectEntry>
                         <ProjectEntry img="https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/store/software/switch/70010000000964/a28a81253e919298beab2295e39a56b7a5140ef15abdb56135655e5c221b2a3a"
                         name="Hello, world!" desc="Goodbye, world!" link="https://github.com/avaerian/Ether"></ProjectEntry>
                         <ProjectEntry img="https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/store/software/switch/70010000000964/a28a81253e919298beab2295e39a56b7a5140ef15abdb56135655e5c221b2a3a"
@@ -50,6 +65,8 @@
 </template>
 
 <script lang="ts" setup>
+import Circle from '~/components/circle.vue';
+
 
 </script>
 
@@ -81,19 +98,16 @@
         border-inline: 1px solid lightgray;
     }
 
-    .intro>.container {
+    #intro>.container {
         background-image:
-            linear-gradient(rgba(0, 119, 255, 0.221) 1px, transparent 1px),
-            linear-gradient(90deg, rgba(0, 119, 255, 0.162) 1px, transparent 1px);
+            linear-gradient(rgba(53, 53, 53, 0.221) 1px, transparent 1px),
+            linear-gradient(90deg, rgba(53, 53, 53, 0.221) 1px, transparent 1px);
         background-size: 1.8em 1.8em;
     }
 
-    /* .intro>.container>.content {
+    /*section>.container>*/.content {
         margin: 28px;
-    } */
 
-    section>.container>.content {
-        margin: 28px;
     }
 
     .cool-tag {
@@ -111,47 +125,116 @@
         transform:translate(-100%);
     }
 
-    .more {
+    #more {
         flex-direction: row;
         justify-content: center;
     }
     
-    .more>.container {
+    #more>.container {
         position: relative;
     }
 
-    .more>.container>.content {
+    #more>.container>.precontent {
+        width: 100%;
         height: 690px;
         /* background: red; */
     }
 
-    .more2>.container>.content {
-        height: 690px;
+    #more>.container>.precontent>.content {
+        position: relative;
+        height: 100%;
+        margin-bottom: 100px;
+        display: flex;
         /* background: blue; */
+        flex-direction: row;
+        justify-content: space-evenly;
+        overflow:hidden;
+        /* background: red; */
     }
 
-    .circle {
-        position:absolute;
-        background:linear-gradient(135deg, #ceffad 1%,#2ad39e 100%);;
-        border-radius:50%;
-        box-shadow: inset 0px 0px 50px -12px rgba(129, 129, 129, 0.208);
-        z-index:3;
+    #more>.container>.precontent>.content>* {
+        width: 48%;
+    }
+    
+    #more>.container>.precontent>.content>.right-content {
+        position: relative;
+        max-width: 500px;
+        max-height: 300px;
+        width: auto;
+        height: auto;
+        background: url("assets/midnight_arrives_debug_photo.png");
     }
 
-    .c1 {
-        width: 120px;
-        height: 120px;
+    #projects>.container {
+        padding-bottom: 80px;
     }
 
-    .projects {
+    .projects-header {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .projects-header>* {
+        vertical-align: middle;
+    }
+
+    .projects-header>a {
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-left: 32px;
+        padding: 8px 16px;
+        border-radius: 50px;
+        border: 1px solid black;
+        text-decoration: none;
+        color: black;
+        transition: 0.3s all;
+        z-index: 10;
+        overflow: hidden;
+    }
+
+    .explore-btn-icon {
+        position: relative;
+        margin-left: 4px;
+    }
+
+    .projects-header>a:hover {
+        color: white;
+    }
+
+    .projects-header>a:before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '';
+        width: 100%;
+        height: 100%;
+        background: black;
+        transition: 0.3s all;
+        z-index: -1;
+        transform: scale(0, 1);
+    }
+
+    .projects-header>a:hover:before {
+        transform: scale(1,1);
+    }
+
+    .project-entries {
         position: relative;
         top: 30px;
         display: flex;
         flex-direction: row;
+        justify-content: center;
+        flex-wrap: wrap;
         row-gap: 24px;
         column-gap: 24px;
-        /* background: red; */
         width: 100%;
         height: 100%;
+    }
+
+    .project-entries>ProjectEntry {
+        display: flexbox;
     }
 </style>
